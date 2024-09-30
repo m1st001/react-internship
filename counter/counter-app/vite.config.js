16afa9d318da0cf0947657ede939eb33b528910f
Mihail <m.a.stepanov@outlook.com>
2024-10-01 02:34:59 +0300

    React.Task1 deploying to Github Pages

--- counter/counter-app/vite.config.js
+++ counter/counter-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: 'https://m1st001.github.io/react-internship/',
   plugins: [react()],
 })
