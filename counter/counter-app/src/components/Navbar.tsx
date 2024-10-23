c53fc2010d4de7c5338fbdd731309de735087047
Mihail <m.a.stepanov@outlook.com>
2024-10-23 18:30:56 +0300

    React.Task5 Refactored Navbar component to use map instead of hardcoded tabs.

--- counter/counter-app/src/components/Navbar.tsx
+++ counter/counter-app/src/components/Navbar.tsx
@@ -2,46 +2,36 @@ import React from "react";
 import { AppBar, Toolbar } from "@mui/material";
 import { NavButton } from "../routes/PageStyles";
 
+interface Tab {
+  name: string;
+  route: string;
+}
+
+const tabs: Tab[] = [
+  { name: "About", route: "#about" },
+  { name: "Counters", route: "#counters" },
+  { name: "Login", route: "#login" },
+  { name: "Login via Redux", route: "#loginviaredux" },
+];
+
 const Navbar = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <NavButton
-          color="inherit"
-          href="#about"
-          sx={{
-            backgroundColor:
-              location.pathname === "/about"
-                ? "rgba(255, 255, 255, 0.2)"
-                : "inherit",
-          }}
-        >
-          About
-        </NavButton>
-        <NavButton
-          color="inherit"
-          href="#counters"
-          sx={{
-            backgroundColor:
-              location.pathname === "/counters"
-                ? "rgba(255, 255, 255, 0.2)"
-                : "inherit",
-          }}
-        >
-          Counters
-        </NavButton>
-        <NavButton
-          color="inherit"
-          href="#login"
-          sx={{
-            backgroundColor:
-              location.pathname === "/login"
-                ? "rgba(255, 255, 255, 0.2)"
-                : "inherit",
-          }}
-        >
-          Login
-        </NavButton>
+        {tabs.map((tab) => (
+          <NavButton
+            color="inherit"
+            href={tab.route}
+            sx={{
+              backgroundColor:
+                location.pathname === tab.route
+                  ? "rgba(255, 255, 255, 0.2)"
+                  : "inherit",
+            }}
+          >
+            {tab.name}
+          </NavButton>
+        ))}
       </Toolbar>
     </AppBar>
   );
